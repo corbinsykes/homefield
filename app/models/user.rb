9d05696dc8b39df58ec7b37161bf950a4be2865f
Corbin Sykes <corbin.sykes@gmail.comm>
2013-07-18 14:48:33 -0400

    Ran migration, removed some stuff from model that was there before

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,5 @@ class User < ActiveRecord::Base
   has_many :bars
   has_one :team
 
-  attr_accessor :email, :password, :password_confirmation
-
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-        # :recoverable, :rememberable, :trackable #, :validatable
+  attr_accessible :name, :email, :password, :password_confirmation
 end
